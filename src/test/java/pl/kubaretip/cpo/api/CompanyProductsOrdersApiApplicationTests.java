b920e0cb94154ed35d7211e28c8f61aeb4c1bc78
Jakub Pitera <pitjakub@gmail.com>
2021-02-01 11:20:03 +0100

    Initial commit

---
+++ src/test/java/pl/kubaretip/cpo/api/CompanyProductsOrdersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kubaretip.cpo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyProductsOrdersApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
