d791c3b796fa17a01146483be31c51c720f04463
Jakub Pitera <pitjakub@gmail.com>
2021-02-08 14:52:51 +0100

    fix: Added default value (false) to column 'deleted' in migration V1.0.5

--- src/main/resources/db/migration/common/V1.0.5__add_category_table.sql
+++ src/main/resources/db/migration/common/V1.0.5__add_category_table.sql
@@ -2,6 +2,6 @@ create table if not exists category
 (
     id      bigint       not null auto_increment,
     name    varchar(150) not null,
-    deleted boolean      not null,
+    deleted boolean      not null default 0,
     primary key (id)
 );
