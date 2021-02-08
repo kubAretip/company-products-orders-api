62de8db04e0c50dc64039457cd6de211cc1e7f43
Jakub Pitera <pitjakub@gmail.com>
2021-02-08 15:01:01 +0100

    Added migration with example categories

---
+++ src/main/resources/db/migration/dev/V9.0.1__add_example_categories_records.sql
@@ -0,0 +1,11 @@
+insert into category (id, name, deleted)
+values (1, 'Office accessories', 0);
+
+insert into category (id, name, deleted)
+values (2, 'Monitors', 0);
+
+insert into category (id, name, deleted)
+values (3, 'Laptops', 0);
+
+insert into category (id, name, deleted)
+values (4, 'Office furniture', 0);
