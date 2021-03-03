273ec8307d6d945e94134297367e23081c55dea5
Jakub Pitera <pitjakub@gmail.com>
2021-03-03 17:13:27 +0100

    fix: Fixed HTTP status. Fixed a bug that case when user is unauthenticated, the response was 403 instead 401.

--- src/main/java/pl/kubaretip/cpo/api/config/SecurityConfig.java
+++ src/main/java/pl/kubaretip/cpo/api/config/SecurityConfig.java
@@ -3,12 +3,14 @@ package pl.kubaretip.cpo.api.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import pl.kubaretip.cpo.api.security.AuthenticationFailureHandler;
 import pl.kubaretip.cpo.api.security.AuthenticationSuccessHandler;
 import pl.kubaretip.cpo.api.constants.AuthoritiesConstants;
@@ -86,6 +88,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic()
                 .disable()
                 .exceptionHandling()
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
         .and()
                 .addFilterBefore(exceptionHandlerFilter, JWTAuthenticationFilter.class)
                 .addFilter(authorizationFilter())
